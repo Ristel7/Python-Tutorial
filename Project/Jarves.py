5c6a5ebe348553f12a563f19e213846508ad9a1d
Priyanshu Kumar <Priyanshurajput7654@gmail.com>
2026-02-07 13:00:07 +0530

    Made Jarvis

--- Project/Jarves.py
+++ Project/Jarves.py
@@ -2,13 +2,12 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+command = "hello" # This would normally come from your microphone
 
-command = "hello"  # This would normally come from your microphone
-
+# Speak Commands
 if "hello" in command:
     speak("Hello Priyanshu, how can I help you with your code today?")
